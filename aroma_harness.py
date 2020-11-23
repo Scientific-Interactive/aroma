2315bef5068532e0853e9afa5dfecc179689c65b
V. Ganesh <tovganesh@acm.org>
2020-11-23 14:13:44 +0530

    initial working test suite

---
+++ aroma_harness.py
@@ -0,0 +1,91 @@
+#
+# aroma_harness.py
+# 
+# Test suite driver
+#
+
+import time
+import fpformat
+
+import aroma
+
+class TestRun:
+  def runAndGetTime(self):
+      pass
+
+  def getName(self):
+      pass
+
+  def getResult(self):
+      pass
+
+  def getExpectedResult(self):
+      pass
+
+  def testPassed(self):
+      pass
+
+class SimpleAromaRunZScan:
+   def __init__(self):
+      self.result = []
+      self.expectedResult = []
+
+   def getName(self):
+      return "SimpleAromRun - z-scan"
+
+   def runAndGetTime(self):
+      start = time.time()
+
+      aroma.aroma("input/benz")
+
+      self.result = open("output/benz.armlog").readlines()
+      self.expectedResult = open("testout/benz.armlog").readlines()
+
+      end = time.time()
+
+      return (end-start)
+
+   def getExpectedResult(self):
+      return repr(self.expectedResult)
+
+   def getResult(self):
+      return repr(self.result)
+
+   def testPassed(self):
+      return self.result == self.expectedResult
+      
+class Harness:
+   def __init__(self):
+      self.numberOfTests = 0
+      self.numberOfTestsPassed = 0
+      self.totalTime = 0.0
+
+   def run(self, testClass):
+      self.numberOfTests += 1
+
+      print("="*50)
+      print("Running Test: " + testClass.getName())
+      testTime = testClass.runAndGetTime()
+      print("\t Time to run test: " +  fpformat.fix(testTime, 4))
+      print("\t Result: \t" + testClass.getResult())
+      print("\t Expected: \t" + testClass.getExpectedResult())
+      print("\t Test passed: \t" + repr(testClass.testPassed()))
+      print("="*50)
+
+      self.totalTime += testTime
+
+      if (testClass.testPassed()):
+         self.numberOfTestsPassed += 1
+
+   def summary(self):
+      print("Total Number of Tests: " + repr(self.numberOfTests))
+      print("Total Number of Tests Passed: " + repr(self.numberOfTestsPassed)) 
+      print("Total Time: " + fpformat.fix(self.totalTime, 4))
+
+if __name__=="__main__":
+   harness = Harness()
+
+   harness.run(SimpleAromaRunZScan())
+
+   harness.summary()
+  
