0f18121b9c166dbb2ec71b73525b69080fd8cfd9
V. Ganesh <tovganesh@acm.org>
2021-01-10 13:23:45 +0530

    a fix of macos installer script

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,5 @@
 rm -fr dist build aroma_macos aroma_macos.tar.gz
-pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' --osx-bundle-identifier=aroma.technion.edu aroma_gui.py
+pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' --hidden-import='tkinter' --osx-bundle-identifier=aroma.technion.edu --add-binary='/System/Library/Frameworks/Tcl.framework/Versions/Current/tclsh8.5':'tcl' aroma_gui.py
 cd dist
 mkdir aroma_macos
 mv Aroma aroma_macos/
