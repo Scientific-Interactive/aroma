cc443e4d47029727ab1f83ec9445ad88f027a425
V. Ganesh <tovganesh@acm.org>
2021-01-11 14:18:56 +0530

    make command line build of linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,6 @@
-rm -fr dist build aroma-linux aroma-linux.tar.gz
+rm -fr dist build aroma-linux aroma-linux.tar.gz aroma-linux-cmd aroma-linux-cmd.tar.gz
+
+# gui build
 pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma_gui.py
 cd dist
 mkdir aroma-linux
@@ -8,5 +10,18 @@ cp ../user_aroma_constants.py aroma-linux/
 cp -r ../tests aroma-linux/
 tar zcvf aroma-linux.tar.gz aroma-linux/
 mv aroma-linux.tar.gz ../
+cd ..
+rm -fr dist build
+
+# cmd build
+pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma.py
+cd dist
+mkdir aroma-linux-cmd
+mv Aroma aroma-linux-cmd/
+cp ../*png aroma-linux-cmd/
+cp ../user_aroma_constants.py aroma-linux-cmd/
+cp -r ../tests aroma-linux-cmd/
+tar zcvf aroma-linux-cmd.tar.gz aroma-linux-cmd/
+mv aroma-linux-cmd.tar.gz ../
 cd ..
 rm -fr dist build
