d483b56fd88d029919bc2b9c84921d400fec3d77
Anuja <anuja@scientificinteractive.com>
2022-01-26 08:59:47 +0100

    Fixed the win-build script for spatial transform rotation groups

--- build_win.sh
+++ build_win.sh
@@ -1,11 +1,10 @@
 rm -fr dist build aroma-win aroma-win.zip
-pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma_gui.py
+pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' --hidden-import='scipy.spatial.transform._rotation_groups' aroma_gui.py
 cd dist
 mkdir aroma-win
 mv Aroma aroma-win/
 cp ../*png aroma-win/
 cp ../user_aroma_constants.py aroma-win/
-cp -r ../tests aroma-win/
 7z a -r aroma-win.zip aroma-win/
 mv aroma-win.zip ../
 cd ..
