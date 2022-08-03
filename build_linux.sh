fc3386de028fc262c304ef35e0c7791eeed9e09d
Anuja <anuja@scientificinteractive.com>
2022-08-03 11:16:50 +0530

    one linux build for cmd, gui and picmo

--- build_linux.sh
+++ build_linux.sh
@@ -1,27 +1,19 @@
-rm -fr dist build aroma-linux aroma-linux.tar.gz aroma-linux-cmd aroma-linux-cmd.tar.gz
+rm -fr dist build aroma-linux aroma-linux.tar.gz aroma-linux aroma-linux.tar.gz
 
-# gui build
-pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma_gui.py
+
+# linux build
+pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma.py
+pyinstaller --windowed --onefile --name Aroma_GUI --hidden-import='PIL._tkinter_finder' aroma_gui.py
+pyinstaller --windowed --onefile --name Aroma_picmo aroma_picmo.py
 cd dist
 mkdir aroma-linux
 mv Aroma aroma-linux/
+mv Aroma_GUI aroma-linux/
+mv Aroma_picmo aroma-linux/
 cp ../*png aroma-linux/
 cp ../user_aroma_constants.py aroma-linux/
 cp -r ../tests aroma-linux/
 tar zcvf aroma-linux.tar.gz aroma-linux/
 mv aroma-linux.tar.gz ../
-cd ..
-rm -fr dist build
-
-# cmd build
-pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma.py
-cd dist
-mkdir aroma-linux-cmd
-mv Aroma aroma-linux-cmd/
-cp ../*png aroma-linux-cmd/
-cp ../user_aroma_constants.py aroma-linux-cmd/
-cp -r ../tests aroma-linux-cmd/
-tar zcvf aroma-linux-cmd.tar.gz aroma-linux-cmd/
-mv aroma-linux-cmd.tar.gz ../
 cd ..
 rm -fr dist build
