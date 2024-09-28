e859bd96d3826bf0dde0d67cf7243c0b27084702
Dr. V. Ganesh <tovganesh@acm.org>
2024-09-28 15:23:12 +0530

    modify build_linux to remove windowed option form command line aroma

--- build_linux.sh
+++ build_linux.sh
@@ -2,9 +2,9 @@ rm -fr dist build aroma-linux aroma-linux.tar.gz aroma-linux aroma-linux.tar.gz
 
 
 # linux build
-pyinstaller --windowed --onefile --name Aroma --hidden-import='PIL._tkinter_finder' --hidden-import=_tkinter aroma.py
 pyinstaller --windowed --onefile --name Aroma_GUI --hidden-import='PIL._tkinter_finder' --hidden-import=_tkinter aroma_gui.py
-pyinstaller --windowed --onefile --name Aroma_picmo aroma_picmo.py
+pyinstaller --onefile --name Aroma --hidden-import='PIL._tkinter_finder' aroma.py
+pyinstaller --onefile --name Aroma_picmo aroma_picmo.py
 cd dist
 mkdir aroma-linux
 mv Aroma aroma-linux/
